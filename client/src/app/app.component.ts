aa05a492ece7330221065f1d1526c463d04ee387
Shiraaz Ahammed <shiraaz.ahammed@gmail.com>
2020-05-03 13:54:48 +0530

    Arrow Key functionality added (Move players with arrow keys)

--- client/src/app/app.component.ts
+++ client/src/app/app.component.ts
@@ -1,5 +1,5 @@
-import { Component, OnInit, ViewChild, ElementRef } from '@angular/core';
-import io from "socket.io-client";
+import { Component, OnInit, ViewChild, ElementRef, Renderer2 } from '@angular/core';
+import io from 'socket.io-client';
 
 @Component({
   selector: 'app-root',
@@ -8,9 +8,9 @@ import io from "socket.io-client";
 })
 export class AppComponent implements OnInit {
 
-  @ViewChild("game")
+  @ViewChild('game')
   private gameCanvas: ElementRef;
-  private context: any;  
+  private context: any;
   private socket: any;
   private mainRect: any;
   private bulletRect: any;
@@ -24,12 +24,12 @@ export class AppComponent implements OnInit {
   }
 
   public ngOnInit(){
-    this.socket = io("http://localhost:3000");
+    this.socket = io('http://localhost:3000');
   }
 
   public ngAfterViewInit(){
-    this.context = this.gameCanvas.nativeElement.getContext("2d");
-    this.socket.on("rectPosition", rectPosition => {
+    this.context = this.gameCanvas.nativeElement.getContext('2d');
+    this.socket.on('rectPosition', rectPosition => {
       this.clearRect();
       this.mainRect = this.context.fillRect(rectPosition.mainX, rectPosition.mainY, 20, 20);
       this.bulletRect = this.context.fillRect(rectPosition.bulletX, rectPosition.bulletY, 5, 5);
@@ -37,7 +37,21 @@ export class AppComponent implements OnInit {
   }
 
   public move(direction: string){
-    this.socket.emit("move", direction);
+    this.socket.emit('move', direction);
   }
-  
+
+  constructor(public el: ElementRef, public renderer: Renderer2) {
+    renderer.listen('document', 'keydown.arrowup', (event) => {
+      return this.move('up');
+    });
+    renderer.listen('document', 'keydown.arrowdown', (event) => {
+      return this.move('down');
+    });
+    renderer.listen('document', 'keydown.arrowright', (event) => {
+      return this.move('right');
+    });
+    renderer.listen('document', 'keydown.arrowleft', (event) => {
+      return this.move('left');
+    });
+ }
 }
