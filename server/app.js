8765ea785078ab41a1ee4aa4da3db9938a49949f
Shiraaz Ahammed <shiraaz.ahammed@gmail.com>
2020-05-07 14:27:03 +0530

    Fix: Removed old code and added a class to distinguish two players

--- server/app.js
+++ server/app.js
@@ -4,85 +4,81 @@ const Socketio = require("socket.io")(Http)
 
 class GamePlayers {
     constructor(p1, p2){
-        this.players = [p1, p2];
-    }
-}
+        this.playerOne = p1;
+        this.playerTwo = p2;
 
-// Player One Position
-var rectPosition = {
-    mainX: 100,
-    mainY: 230,
-    bulletX: 105,
-    bulletY: 235,
-    secondX: 460,
-    secondY: 230,
-    secondBulletX: 455,
-    secondBulletY: 225
-};
+        this.rectPosition = {
+            mainX: 100,
+            mainY: 230,
+            bulletX: 105,
+            bulletY: 235,
+            secondX: 460,
+            secondY: 230,
+            secondBulletX: 455,
+            secondBulletY: 225
+        };
+
+        Socketio.emit("rectPosition", this.rectPosition);
+
+        this.playerOne.on('move', data => {
+                    switch(data) {
+                        // Player Movement
+                        case "up":
+                            this.rectPosition.mainY = this.rectPosition.mainY - 10;
+                            this.rectPosition.bulletY = this.rectPosition.bulletY - 10;
+                            Socketio.emit("rectPosition", this.rectPosition);
+                            break;
+                        case "down":
+                            this.rectPosition.mainY = this.rectPosition.mainY + 10;
+                            this.rectPosition.bulletY = this.rectPosition.bulletY + 10;
+                            Socketio.emit("rectPosition", this.rectPosition);
+                            break;
+                            //Player Shooting.
+                        case "shoot":
+                            setInterval(this.shooting, 10);
+                            setInterval(() => {
+                                Socketio.emit("rectPosition", rectPosition);
+                            }, 100);
+                    }
+                })
+        
+        this.playerTwo.on('move', data => {
+                    switch(data) {
+                        // Player Movement
+                        case "up":
+                            this.rectPosition.secondY = this.rectPosition.secondY - 10;
+                            this.rectPosition.secondBulletY = this.rectPosition.secondBulletY - 10;
+                            Socketio.emit("rectPosition", this.rectPosition);
+                            break;
+                        case "down":
+                            this.rectPosition.secondY = this.rectPosition.secondY + 10;
+                            this.rectPosition.secondBulletY = this.rectPosition.secondBulletY + 10;
+                            Socketio.emit("rectPosition", this.rectPosition);
+                            break;
+                            //Player Shooting.
+                        case "shoot":
+                            setInterval(this.shooting, 10);
+                            setInterval(() => {
+                                Socketio.emit("rectPosition", rectPosition);
+                            }, 100);
+                    }
+                })
+            }; 
+    }
 
 let waitingPlayer = null;
 
-//Player One shooting Function
-function shooting() {
-    return rectPosition.bulletX++
-}
 
 // On Connection
 Socketio.on("connection", socket => {
     
     if(waitingPlayer) {
-        socket.emit("msg", 'Got opponent');
-        waitingPlayer.emit("msg", 'Got opponent')
-            //Sending players Position
-        Socketio.emit("rectPosition", rectPosition);
+        new GamePlayers(waitingPlayer, socket);
+        waitingPlayer = null;
     } else {
         waitingPlayer = socket;
         waitingPlayer.emit("msg", 'Waiting For opponent');
     }
-    // Code For Player Movement And Shooting
-    socket.on("move", data => {
-        switch(data) {
-            // Player Movement
-            case "up":
-                rectPosition.mainY = rectPosition.mainY - 10;
-                rectPosition.bulletY = rectPosition.bulletY - 10;
-                Socketio.emit("rectPosition", rectPosition);
-                break;
-            case "down":
-                rectPosition.mainY = rectPosition.mainY + 10;
-                rectPosition.bulletY = rectPosition.bulletY + 10;
-                Socketio.emit("rectPosition", rectPosition);
-                break;
-                //Player Shooting.
-            case "shoot":
-                setInterval(shooting, 10);
-                setInterval(() => {
-                    Socketio.emit("rectPosition", rectPosition);
-                }, 100);
-        }
-    })
-
-    waitingPlayer.on("move", data => {
-        switch(data) {
-            // Player Movement
-            case "up":
-                rectPosition.secondY = rectPosition.secondY - 10;
-                rectPosition.secondBulletY = rectPosition.secondBulletY - 10;
-                Socketio.emit("rectPosition", rectPosition);
-                break;
-            case "down":
-                rectPosition.secondY = rectPosition.secondY + 10;
-                rectPosition.secondBulletY = rectPosition.secondBulletY + 10;
-                Socketio.emit("rectPosition", rectPosition);
-                break;
-                //Player Shooting.
-            case "shoot":
-                setInterval(shooting, 10);
-                setInterval(() => {
-                    Socketio.emit("rectPosition", rectPosition);
-                }, 100);
-            }
-        })
 })
 // Listen to server. 
 Http.listen(3000, () => {
