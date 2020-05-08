cfc513391411944490ea5c6aa2e7879b10a563c6
Shiraaz Ahammed <shiraaz.ahammed@gmail.com>
2020-05-08 10:40:30 +0530

    Removed game logic from server, and added the JS File Path of Game logic

--- server/app.js
+++ server/app.js
@@ -2,73 +2,7 @@ const Express = require("express")();
 const Http = require("http").Server(Express);
 const Socketio = require("socket.io")(Http)
 
-class GamePlayers {
-    constructor(p1, p2){
-        this.playerOne = p1;
-        this.playerTwo = p2;
-
-        this.rectPosition = {
-            mainX: 100,
-            mainY: 230,
-            bulletX: 105,
-            bulletY: 235,
-            secondX: 460,
-            secondY: 230,
-            secondBulletX: 455,
-            secondBulletY: 225
-        };
-
-        this.shooting = () => {
-                return this.rectPosition.bulletX++;
-        }
-
-        Socketio.emit("rectPosition", this.rectPosition);
-
-        this.playerOne.on('move', data => {
-                    switch(data) {
-                        // Player Movement
-                        case "up":
-                            this.rectPosition.mainY = this.rectPosition.mainY - 10;
-                            this.rectPosition.bulletY = this.rectPosition.bulletY - 10;
-                            Socketio.emit("rectPosition", this.rectPosition);
-                            break;
-                        case "down":
-                            this.rectPosition.mainY = this.rectPosition.mainY + 10;
-                            this.rectPosition.bulletY = this.rectPosition.bulletY + 10;
-                            Socketio.emit("rectPosition", this.rectPosition);
-                            break;
-                            //Player Shooting.
-                        case "shoot":
-                            setInterval(this.shooting, 10);
-                            setInterval(() => {
-                                Socketio.emit("rectPosition", this.rectPosition);
-                            }, 100);
-                    }
-                })
-        
-        this.playerTwo.on('move', data => {
-                    switch(data) {
-                        // Player Movement
-                        case "up":
-                            this.rectPosition.secondY = this.rectPosition.secondY - 10;
-                            this.rectPosition.secondBulletY = this.rectPosition.secondBulletY - 10;
-                            Socketio.emit("rectPosition", this.rectPosition);
-                            break;
-                        case "down":
-                            this.rectPosition.secondY = this.rectPosition.secondY + 10;
-                            this.rectPosition.secondBulletY = this.rectPosition.secondBulletY + 10;
-                            Socketio.emit("rectPosition", this.rectPosition);
-                            break;
-                            //Player Shooting.
-                        case "shoot":
-                            setInterval(this.shooting, 10);
-                            setInterval(() => {
-                                Socketio.emit("rectPosition", this.rectPosition);
-                            }, 100);
-                    }
-                })
-            }; 
-    }
+const GamePlayers = require('./game-players');
 
 let waitingPlayer = null;
 
